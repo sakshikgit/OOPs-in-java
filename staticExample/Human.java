324d5b583983389675d71a4173cf6d1cd19db660
sakshigmail <sakshi3016kaurav.com>
2025-03-25 17:57:44 +0530

    message

--- staticExample/Human.java
+++ staticExample/Human.java
@@ -1,5 +1,19 @@
 package staticExample;
 
 public class Human {
+    int age;
+    String name;
+    int Salary;
+    boolean married;
+    static long population;
     
+
+
+public Human(int age, String name, int Salary, boolean married){
+    this.age = age;
+    this.name = name;
+    this.Salary = Salary;
+    this.married = married;
+    Human.population += 1;
+}
 }
--- staticExample/Main.java
+++ staticExample/Main.java
@@ -1,5 +1,15 @@
 package staticExample;
 
 public class Main {
+    public static void main(String[] args) {
+        Human sakshi = new Human(20, "sakshi", 100000, false);
+        Human sakhi = new Human(20, "sakhi", 100000, true);
+
+        System.out.println(Human.population);
+
+   
+    
+    
+    }
 
 }
