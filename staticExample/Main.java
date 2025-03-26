33419ababc0245dbe731ba6b73730cfab135794c
sakshigmail <sakshi3016kaurav.com>
2025-03-26 08:10:23 +0530

    changes

--- staticExample/Main.java
+++ staticExample/Main.java
@@ -3,14 +3,25 @@ import staticExample.Human;
 
 public class Main {
     public static void main(String[] args) {
-        Human sakshi = new Human(20, "sakshi", 100000, false);
-        Human sakhi = new Human(20, "sakhi", 100000, true);
+        // Human sakshi = new Human(20, "sakshi", 100000, false);
+        // Human sakhi = new Human(20, "sakhi", 100000, true);
 
-        System.out.println(Human.population);
+        // System.out.println(Human.population);
 
-   
-    
-    
+        
+    }
+
+// this is not dependent on objects
+    static void fun(){
+        greeting(); // you can't use this because it requires an instance
+        // but the function you are using it in does not depend on instances 
+
+    }
+
+
+    // we know that something which is not static, belong to an object
+    void greeting(){
+        System.out.println("Hello world");
     }
 
 }
