56f095e6f5d2a2ac48c18fd415dbc73b1f0204d2
sakshigmail <sakshi3016kaurav.com>
2025-03-29 12:01:40 +0530

    changes

---
+++ Properties/inheritance/Main.java
@@ -0,0 +1,10 @@
+package Properties.inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Box box = new Box();
+
+        System.out.println(box.l + " " + box.w + " " + box.h);
+    }
+    
+}
---
+++ staticExample/InnerClasses.java
@@ -0,0 +1,19 @@
+package staticExample;
+
+public class InnerClasses {
+    class Test{
+        String name;
+
+        public Test(String name){
+            this.name = name;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Test a = new Test("sakshi");
+        
+    }
+    
+    
+}
